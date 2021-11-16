c08f64e60e268881bbbc31307a7ed60f1847a4f1
falconsmilie <heizenburgblue@gmail.com>
2021-11-16 17:04:16 +0100

    GildedRose: update unit test for GildedRose to 100% coverage

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -20,14 +20,22 @@ class GildedRoseTest extends TestCase
 
     public function updateQualityDataProvider(): array
     {
-        // $name, $sellIn, $quality, $expectedQuality
+        // $name, $sellIn, $expectedSellIn, $quality, $expectedQuality
         return [
             ['+5 Dexterity Vest', 10, 9, 20, 19],
+            ['+5 Dexterity Vest', 0, -1, 20, 18],
             ['Aged Brie', 2, 1, 2, 3],
+            ['Aged Brie', -2, -3, 2, 4],
             ['Elixir of the Mongoose', 5, 4, 7, 6],
+            ['Elixir of the Mongoose', -5, -6, 7, 5],
             ['Sulfuras, Hand of Ragnaros', 2, 2, 80, 50],
+            ['Sulfuras, Hand of Ragnaros', -2, -2, 80, 50],
             ['Sulfuras, Hand of Ragnaros', 2, 2, 20, 20],
+            ['Sulfuras, Hand of Ragnaros', -2, -2, 20, 20],
             ['Backstage passes to a TAFKAL80ETC concert', 15, 14, 20, 21],
+            ['Backstage passes to a TAFKAL80ETC concert', 0, -1, 20, 0],
+            ['Backstage passes to a TAFKAL80ETC concert', -10, -11, 50, 0],
+            ['Backstage passes to a TAFKAL80ETC concert', -10, -11, 150, 0],
             ['Backstage passes to a TAFKAL80ETC concert', 10, 9, 48, 50],
             ['Backstage passes to a TAFKAL80ETC concert', 10, 9, 49, 50],
             ['Backstage passes to a TAFKAL80ETC concert', 5, 4, 49, 50],
